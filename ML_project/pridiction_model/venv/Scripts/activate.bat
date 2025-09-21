eb060d895d680bfd738004fd1ed2a7c67ba6b79d
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-09-22 01:34:28 +0530

    update

--- ML_project/pridiction_model/venv/Scripts/activate.bat
+++ ML_project/pridiction_model/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\AI\nine_MACHINE LEARNING\1_linearRegression\venv
+set VIRTUAL_ENV=C:\AI\nine_MACHINE LEARNING\ML_project\pridiction\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
